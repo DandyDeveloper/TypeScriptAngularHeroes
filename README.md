d9ace497bba504a4910436873dd01b38532e928e
Aaron Layfield <Aaron.Layfield@gmail.com>
2016-10-23 17:43:07 +0900

    Removed npm debug, added README

---
+++ README.md
@@ -0,0 +1,4 @@
+#Basic Angular app for displaying a list of League heroes
+To use this, simply clone, run `npm install` then `npm start`. 
+
+This should open BrowserSync and you can make changes changes and play. 
--- app/app.component.ts
+++ app/app.component.ts
@@ -6,20 +6,20 @@ export class Hero {
  }
 
 const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Aatrox' },
+  { id: 12, name: 'Jhin' },
+  { id: 13, name: 'Rammus' },
+  { id: 14, name: 'Annie' },
+  { id: 15, name: 'Zed' },
+  { id: 16, name: 'Lucian' },
+  { id: 17, name: 'Thresh' },
+  { id: 18, name: 'Gnar' },
+  { id: 19, name: 'Ryze' },
+  { id: 20, name: 'Jayce' }
 ];
 
 @Component({
-  selector: 'my-app',
+  selector: 'heroes-list',
   template: `
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
